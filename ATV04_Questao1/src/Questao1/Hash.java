39b535570a964d2082d0be45e3fd9c2036b1bf2d
Eduardo Cadilhe <eduardo.00.1du@gmail.com>
2024-04-29 17:08:48 -0300

    modified hashValue

--- ATV04_Questao1/src/Questao1/Hash.java
+++ ATV04_Questao1/src/Questao1/Hash.java
@@ -1,11 +1,11 @@
-package Questao1_AgainDenovo;
+package Questao1;
 
 public class Hash {
 
     public static int of(String frase){
         int hashValue = 0;
         for (int i = 0; i < frase.length(); i++) {
-            hashValue = (((int)Math.pow(frase.charAt(i),i) * 31)  + hashValue);
+            hashValue = (((int)Math.pow(frase.charAt(i),i+1) * 31)  + hashValue);
         }
         return Math.abs(hashValue);
     }
